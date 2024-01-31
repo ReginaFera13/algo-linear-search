24d4807424aa6bdb745da0a4a64ae7754b7e0fa8
SHAWN_SMITH <141180011+ReginaFera13@users.noreply.github.com>
2024-01-31 17:17:28 -0500

    linear search solved

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,27 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  let index = undefined
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == searchTerm) {
+      index = i
+    }
+   
+  }
+  return index
 }
 
+console.log(linearSearch(4, [1, 2, 3]))
+
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let index = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == searchTerm) {
+      index.push(i)
+    }
+   
+  }
+  return index
 }
+
+console.log(globalLinearSearch("n", "bananas".split("")))
 
 module.exports = { linearSearch, globalLinearSearch };
